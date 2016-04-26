6dc0e5ffb12826f1c61b0db8066bef02184bd6ed
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-04-26 09:19:41 -0700

    Change SPL code to new topics

--- test/build.sh
+++ test/build.sh
@@ -13,11 +13,11 @@ sc -a -M com.ibm.streamsx.iot.watson.apps::SimpleAllDevices
 
 export STREAMS_SPLPATH=$BASE_SPLPATH:$BASE/samples/CountEvents
 cd .. ; rm -fr apptest ; mkdir apptest ; cd apptest
-sc -a -M com.ibm.streamsx.iotf.sample.countevents::CountEvents
+sc -a -M com.ibm.streamsx.iot.sample.countevents::CountEvents
 
 export STREAMS_SPLPATH=$BASE_SPLPATH:$BASE/samples/Quarks
 cd .. ; rm -fr apptest ; mkdir apptest ; cd apptest
-sc -a -M com.ibm.streamsx.iotf.sample.quarks::IotfSensors
+sc -a -M com.ibm.streamsx.iot.sample.quarks::IotfSensors
 
 cd .. ; rm -fr apptest
 echo "All applications compiled OK"
